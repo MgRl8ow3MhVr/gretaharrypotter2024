63a6ec45010c84451ff02082710ee1871e8a6a33
mother4 <mother4@mother4s-MacBook-Pro.local>
2024-11-15 10:23:49 +0100

    etape 3

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -12,11 +12,25 @@ const Characters = (props) => {
   useEffect(() => {
     // on définit une fonction asynchrone ici car useEffect ne peut pas prendre en entrée une fonction asynchrone
     const fetchData = async () => {
-      const reponse = await axios.get(
-        "https://hp-api.herokapp.com/api/characters"
-      );
-      // mise à jour du state avec les data, cela va déclencher un rafraichissement du component
-      setData(reponse.data);
+      try {
+        const response = await axios.get(
+          "https://hp-api.herokuapp.com/api/characters"
+        );
+        console.log("response", response);
+
+        // On peut aussi forcer à aller dans le "catch" dans le cas ou on veut forcer une erreur
+        // par exemple, nous voudrions aller dans l'erreur si la longueur du tableau de réponse était trop longue
+        // if (response.data.length > 100) {
+        //   throw new Error("il y a eu trop de resulstats");
+        // }
+        // mise à jour du state avec les data, cela va déclencher un rafraichissement du component
+        setData(response.data);
+
+        console.log(response.data.length);
+      } catch (error) {
+        console.log(error);
+        alert("désolé, il y a eu une erreur pendant l'appel de données");
+      }
     };
 
     // Execution de fetchData
@@ -29,8 +43,8 @@ const Characters = (props) => {
       Ici, si data existe (une fois qu'il a été mis à jour dans le useeffect, alors dérouler dans une liste .map) Mais si il est Null (au montage du component quand l'appel de data est en cours, afficher un message "en attente", ou une roue d'attente)
       */}
       {data ? (
-        data.map((character) => {
-          return <Card character={character} />;
+        data.map((character, i) => {
+          return <Card character={character} key={i} />;
         })
       ) : (
         <div>En attente</div>
